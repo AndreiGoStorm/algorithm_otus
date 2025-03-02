2c333b1030758bae3a4c3aafd570e142437172aa
Andrzej Makaś <a.makas@riot.agency>
2025-03-02 08:54:56 +0100

    HW1 is completed

--- hw1/main.go
+++ hw1/main.go
@@ -2,20 +2,110 @@ package main
 
 import "fmt"
 
-func main() {
-	pics_01()
-}
+const N = 25
 
-func pics_01() {
-	N := 25
+func main() {
 	for x := 0; x < N; x++ {
 		for y := 0; y < N; y++ {
-			if x < y {
-				fmt.Print("* ")
-			} else {
-				fmt.Print(". ")
-			}
+			pics_01(x, y)
+			pics_02(x, y)
+			pics_03(x, y)
+			pics_04(x, y)
+			pics_05(x, y)
+			pics_06(x, y)
+			pics_07(x, y)
+			pics_08(x, y)
+			pics_09(x, y)
+			pics_10(x, y)
+			pics_11(x, y)
+			pics_18(x, y)
+			pics_19(x, y)
+			pics_20(x, y)
+			pics_21(x, y)
+			pics_23(x, y)
+			pics_24(x, y)
+			pics_25(x, y)
 		}
 		fmt.Println("")
+	}
+}
+
+func pics_01(x, y int) {
+	show(x < y)
+}
+
+func pics_02(x, y int) {
+	show(x == y)
+}
+
+func pics_03(x, y int) {
+	show(y == N-x-1)
+}
+
+func pics_04(x, y int) {
+	show(y <= N-x+4)
+}
+
+func pics_05(x, y int) {
+	show(y == 2*x+1 || y == 2*(x-1)+2)
+}
+
+func pics_06(x, y int) {
+	show(x < (N-4)/2 || y < (N-4)/2)
+}
+
+func pics_07(x, y int) {
+	show(x > (N+5)/2 && y > (N+5)/2)
+}
+
+func pics_08(x, y int) {
+	show(x == 0 || y == 0)
+}
+
+func pics_09(x, y int) {
+	show(x-(N-4)/2 > y || x+(N-4)/2 < y)
+}
+
+func pics_10(x, y int) {
+	show(x < y && y < 2*x+2)
+}
+
+func pics_11(x, y int) {
+	show((!(x == 0 && y == 0) && (x == 1 || y == 1)) || (!(x == N-1 && y == N-1) && (x == N-2 || y == N-2)))
+}
+
+func pics_18(x, y int) {
+	show(((x != 0 || y != 0) && (x == 0 || y == 0)) || (x == 1 || y == 1))
+}
+
+func pics_19(x, y int) {
+	show((x == 0 || y == 0) || (x == N-1 || y == N-1))
+}
+
+func pics_20(x, y int) {
+	show(x%2 == y%2)
+}
+
+func pics_21(x, y int) {
+	show(y%(x+1) == 0)
+}
+
+func pics_23(x, y int) {
+	show(x%3 == 0 && y%2 == 0)
+}
+
+func pics_24(x, y int) {
+	show(x == y || y == N-x-1)
+}
+
+func pics_25(x, y int) {
+	show(x%6 == 0 || y%6 == 0)
+}
+
+func show(condition bool) {
+	if condition {
+		fmt.Print("* ")
+	} else {
+		fmt.Print(". ")
 	}
 }
